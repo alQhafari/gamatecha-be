59eb27725fb1b8d6256f56d1f92459abb7f25e71
abiQhafari <qhafari123@gmail.com>
2025-05-04 20:40:21 +0700

    refactor(ecosystem): update application configuration for improved startup
    Signed-off-by: abiQhafari <qhafari123@gmail.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'gamatecha-be', // Nama aplikasi
-      script: 'dist/main.js', // Lokasi file hasil build NestJS
-      exec_mode: 'fork', // Menjalankan aplikasi dalam mode fork
-      instances: 1, // Jumlah instance aplikasi (1 untuk single instance, gunakan 'max' untuk otomatis menyesuaikan jumlah CPU)
-      autorestart: true, // Otomatis restart aplikasi jika terjadi crash
-      watch: false, // Menonaktifkan watch (pengawasan terhadap perubahan file, set true jika ingin memonitor file)
-      max_memory_restart: '1G', // Maksimum penggunaan memori sebelum restart aplikasi
+      name: 'gamatecha-be',
+      script: 'pnpm',
+      args: 'run start',
+      watch: false,
       env: {
-        NODE_ENV: 'development', // Environment untuk development
-        PORT: 3001, // Port aplikasi berjalan
+        NODE_ENV: 'development',
+        PORT: 3001,
       },
       env_production: {
-        NODE_ENV: 'production', // Environment untuk production
-        PORT: 3001, // Port aplikasi berjalan
+        NODE_ENV: 'production',
+        PORT: 3001,
       },
     },
   ],
